2079e96075db6938167f772e7daf0f5aa9e51a35
androidgeek077 <vickylucky088@gmail.com>
2019-07-15 23:02:36 -0700

    date 15-7-2019

--- ClassTenAmDemoApp/app/src/main/java/app/techsol/classtenamdemoapp/MainActivity.java
+++ ClassTenAmDemoApp/app/src/main/java/app/techsol/classtenamdemoapp/MainActivity.java
@@ -3,6 +3,10 @@ package app.techsol.classtenamdemoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Snackbar.make(findViewById(R.id.getView),"I'm snackbar", Snackbar.LENGTH_INDEFINITE).setAction("ok", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "I'm clicked", Toast.LENGTH_SHORT).show();
+            }
+        }).show();
     }
 }
