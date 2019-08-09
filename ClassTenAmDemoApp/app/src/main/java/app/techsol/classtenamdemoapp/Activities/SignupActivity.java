a0e94bbecc0fddbb893048f56479ba66b7997f1e
androidgeek077 <vickylucky088@gmail.com>
2019-08-09 06:36:27 -0700

    progress bar added

--- ClassTenAmDemoApp/app/src/main/java/app/techsol/classtenamdemoapp/Activities/SignupActivity.java
+++ ClassTenAmDemoApp/app/src/main/java/app/techsol/classtenamdemoapp/Activities/SignupActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -14,6 +15,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.kaopiz.kprogresshud.KProgressHUD;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +26,9 @@ public class SignupActivity extends AppCompatActivity {
 
     EditText mEmailET, mPasswordET;
     Button mLoginBtn;
+    ProgressBar mProgressBar;
+    private KProgressHUD kProgressHUD;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +36,28 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         mEmailET=findViewById(R.id.emailET);
         mPasswordET=findViewById(R.id.passwordET);
+        mProgressBar=findViewById(R.id.myProgressBar);
         mLoginBtn=findViewById(R.id.loginBtn);
         mLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                StringRequest myStringRequest=new StringRequest(1, "https://malikwaqas077.000webhostapp.com/android/signup.php", new Response.Listener<String>() {
+                kProgressHUD = KProgressHUD.create(SignupActivity.this)
+                        .setStyle(KProgressHUD.Style.SPIN_INDETERMINATE)
+                        .setAnimationSpeed(2)
+                        .setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark))
+                        .setLabel("Adding")
+                        .setDetailsLabel("Please Wait...")
+                        .setDimAmount(0.3f)
+                        .show();
+                StringRequest myStringRequest=new StringRequest(1, "https://endogamic-adverb.000webhostapp.com/andorid/signup.php", new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        Toast.makeText(SignupActivity.this, response, Toast.LENGTH_SHORT).show();
+                        kProgressHUD.dismiss();                        Toast.makeText(SignupActivity.this, response, Toast.LENGTH_SHORT).show();
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
+//                        mProgressBar.setVisibility(View.GONE);
                         Toast.makeText(SignupActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 }){
